fde65c6aaca51173173e0618e8317e79bafdc1a4
Marco Smith <newmarkoiskewl@gmail.com>
2018-07-18 13:00:51 -0400

    Added save feature (In Development)

--- app/src/main/java/com/example/marcosmith/whacamole/HomeActivity.java
+++ app/src/main/java/com/example/marcosmith/whacamole/HomeActivity.java
@@ -1,28 +1,63 @@
 package com.example.marcosmith.whacamole;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button playButton;
+    public TextView streakNumber, scoreNumber;
+
+    public static int highScore = MainActivity.points;
+    public static int highstreak = MainActivity.streak;
+
+    private final String SCORE_KEY = "scorekey";
+    private final String STREAK_KEY = "streakkey";
+
+    public static String pointsString;
+    public String testsave = "yo";
+
+    //public SharedPreferences loadpoints = MainActivity.load;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-/*        playButton.findViewById(R.id.button);
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });*/
+/*        loadpoints = getSharedPreferences(pointsString, 0);
+        String mString = loadpoints.getString("HighScore", "0");*/
+
+/*        scoreNumber = findViewById(R.id.textView11);
+        scoreNumber.setText(mString);*/
+        Log.d("logtag", "Main Activity booted");
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button){
+            Intent homeIntent = new Intent(HomeActivity.this,MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+            Log.d("logtag", "Main Activity fired");
+        }
     }
 
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();{
+        }
+    }
 }
+
+
